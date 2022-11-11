233b08ed509311ea85bc08b9518c7faea9b99185
Robert Kein <robertkejn@gmail.com>
2022-11-11 22:01:41 +0400

    1

---
+++ ht_2.2/ht_2.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+struct User
+{
+    long long AccountNumber = 0;
+    std::string Name = "";
+    double Balance = 0;
+};
+
+void ChangeBalance(User& user, double newBalance) {
+    user.Balance = newBalance;
+}
+
+void ViewAccout(User& user) {
+    std::cout << "Ваш счёт : " << user.Name << ", " << user.AccountNumber << ", " << user.Balance << "\n";
+}
+
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+    User user;
+    double newBalance = 0;
+    std::cout << "Введите номер счета : ";
+    std::cin >> user.AccountNumber;
+    std::cout << "Введите имя владельца : ";
+    std::cin >> user.Name;
+    std::cout << "Введите баланс : ";
+    std::cin >> user.Balance;
+    std::cout << "Введите новый баланс : ";
+    std::cin >> newBalance;
+    ChangeBalance(user, newBalance);
+    ViewAccout(user);
+}
